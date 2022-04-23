af5d6158b26fa0475a1368aceef2561d52ebd2cd
dormancy (Dr. ) <cdorm@g.ecc.u-tokyo.ac.jp>
2022-04-23 21:34:13 +0000

    Version 5.1.0.9001 Trial 1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lefko3@79 91a9b8db-8cb9-46d9-b69c-8c1c3c0b293e

--- pkg/R/popdyn.R
+++ pkg/R/popdyn.R
@@ -4122,6 +4122,10 @@ summary.lefkoLTRE <- function(object, ...) {
 #' @param object A \code{lefkoProj} object.
 #' @param threshold A threshold population size to be searched for in
 #' projections. Defaults to 1.
+#' @param inf_alive A logical value indicating whether to treat infinitely
+#' large population size as indicating that the population is still extant.
+#' If \code{FALSE}, then the population is considered extinct. Defaults to
+#' \code{TRUE}.
 #' @param milepost A numeric vector indicating at which points in the projection
 #' to assess detailed results. Can be input as integer values, in which case
 #' each number must be between 1 and the total number of occasions projected in
@@ -4134,6 +4138,21 @@ summary.lefkoLTRE <- function(object, ...) {
 #' the output:
 #' \item{milepost_sums}{A data frame showing the number of replicates at each
 #' of the milepost times that is above the threshold population/patch size.}
+#' 
+#' @section Notes:
+#' 
+#' The \code{inf_alive} option assesses whether replicates have reached a value
+#' of \code{NaN}. If \code{inf_alive = TRUE} and a value of \code{NaN} is found,
+#' then the replicate is considered extant. If the setting is
+#' \code{inf_alive = FALSE}, then a value of \code{NaN} is considered evidence
+#' of extinction. Generally, leaving this option at its default setting works
+#' well because the most common situation in which a value of \code{NaN} is
+#' incorporated into a projection is because the population size reaches a value
+#' above the computational maximum. However, it is theoretically possible that
+#' an unusually small but positive number may also produce this result, in which
+#' case the population should be interpreted as having gone extinct. Users
+#' should always inspect their projections thoroughly to determine the proper
+#' interpretation.
 #' 
 #' @examples
 #' # Lathyrus example
@@ -4238,7 +4257,7 @@ summary.lefkoLTRE <- function(object, ...) {
 #' summary(cypstoch)
 #' 
 #' @export
-summary.lefkoProj <- function(object, threshold = 1,
+summary.lefkoProj <- function(object, threshold = 1, inf_alive = TRUE,
   milepost = c(0, 0.25, 0.50, 0.75, 1.00), ...) {
   
   poppatches <- length(object$pop_size)
@@ -4262,14 +4281,26 @@ summary.lefkoProj <- function(object, threshold = 1,
   if (nreps > 1) {
     milepost_sums <- apply(as.matrix(c(1:poppatches)), 1, function (X) {
       phew <- apply(as.matrix(object$pop_size[[X]][,milepost]), 2, function(Y) {
-        return(length(which(as.vector(Y) >= threshold)))
+        above_vector <- which(as.vector(Y) >= threshold)
+        
+        if (inf_alive) {
+          inf_runs <- which(is.nan(as.vector(Y)))
+          above_vector <- union(above_vector, inf_runs)
+        }
+        return(length(above_vector))
       })
       return(phew)
     })
   } else {
      milepost_sums <- apply(as.matrix(c(1:poppatches)), 1, function (X) {
       phew <- apply(as.matrix(object$pop_size[[X]][,milepost]), 1, function(Y) {
-        return(length(which(as.vector(Y) >= threshold)))
+        above_vector <- which(as.vector(Y) >= threshold)
+        
+        if (inf_alive) {
+          inf_runs <- which(is.nan(as.vector(Y)))
+          above_vector <- union(above_vector, inf_runs)
+        }
+        return(length(above_vector))
       })
       return(phew)
     })
